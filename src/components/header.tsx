875928ecdb5d0207eab8000b5918b08c72217a60
Guilherme Araujo <guidrop17@gmail.com>
2024-07-30 23:29:40 -0300

    Fix: CI Cypress

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 
 import { CartWidget } from './cart-widget'
 import { SearchForm } from './search-form'
+import { Suspense } from 'react'
 
 export function Header() {
   return (
@@ -11,8 +12,9 @@ export function Header() {
         <Link href="/" className="text-2xl font-extrabold text-white">
           CoalaTech
         </Link>
-
-        <SearchForm />
+        <Suspense>
+          <SearchForm />
+        </Suspense>
       </div>
 
       <div className="flex items-center gap-4">
